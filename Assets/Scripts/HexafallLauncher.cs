420147f906f91debff70029962aa0da625445d83
JaumeMontagut <jaume.montagut.i.guix@gmail.com>
2021-01-10 23:22:25 +0100

    creating menus and upgrading materials

---
+++ Assets/Scripts/HexafallLauncher.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class HexafallLauncher : MonoBehaviourPunCallbacks
+{
+    [SerializeField] InputField roomInputField;
+    
+    //ErrorMenu
+    [SerializeField] Text errorText;
+
+    private void Start()
+    {
+        Debug.Log("Connecting to Master 1");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master 2");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined lobby");
+        MenuManager.Instance.OpenMenu("TitleMenu");
+    }
+
+    public void CreateRoom()
+    {
+        if (string.IsNullOrEmpty(roomInputField.text))
+        {
+            Debug.LogError("Invalid room name.");
+            return;
+        }
+        PhotonNetwork.CreateRoom(roomInputField.text);
+        MenuManager.Instance.OpenMenu("LoadingMenu");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        MenuManager.Instance.OpenMenu("RoomMenu");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        errorText.text = "Room creation failed: " + message;
+        MenuManager.Instance.OpenMenu("ErrorMenu");
+    }
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager Instance;
+
+    [SerializeField] GameObject[] menus;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void OpenMenu (string menuName)
+    {
+        foreach (GameObject m in menus)
+        {
+            m.SetActive(false);
+        }
+
+        GameObject menu = Array.Find(menus, m => m.name == menuName);
+        if (menu == null)
+        {
+            Debug.LogWarning("Menu with name " + menuName + " not found on the menu array");
+        }
+        else
+        {
+            menu.SetActive(true);
+        }
+    }
+}
