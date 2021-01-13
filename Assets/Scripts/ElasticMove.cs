fd13e008e6b9a48d7dc63f1aceef8c73b1aa830a
Yessica Servin Dominguez <36170847+YessicaSD@users.noreply.github.com>
2021-01-13 14:58:16 +0100

    adding star

---
+++ Assets/Scripts/ElasticMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElasticMove : MonoBehaviour
+{
+    bool moveDown = true;
+    Vector3 startPosition;
+    Vector3 maxDownPos;
+    float berpValue = 0;
+    public float maxDownValue =  0.5f;
+    public float verticalSpeed = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        maxDownPos = new Vector3(startPosition.x, startPosition.y - maxDownValue, startPosition.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moveDown)
+        {
+            transform.position -= Vector3.up * verticalSpeed * Time.deltaTime;
+            if (transform.position.y <= maxDownPos.y)
+                moveDown = false;
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, Berp(maxDownPos.y, startPosition.y, berpValue), transform.position.z);
+            berpValue += 1 * Time.deltaTime; 
+        }
+    }
+    public static float Berp(float start, float end, float value)
+    {
+        value = Mathf.Clamp01(value);
+        value = (Mathf.Sin(value * Mathf.PI * (0.5f + 3.0f * value * value * value)) * Mathf.Pow(1f - value, 2.2f) + value) * (1f + (1.2f * (1f - value)));
+        return start + (end - start) * value;
+    }
+}
