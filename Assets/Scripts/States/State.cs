9f5ad340c17cd905443d4a0afcc6feccba061ddb
AX3RT (Aurelio) <aurelioart64@gmail.com>
2021-01-25 20:44:11 +0100

    Pushed Fucking Metas

---
+++ Assets/Scripts/States/State.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public abstract class State
+{
+    protected StateMachine stateMachine;
+    protected State(StateMachine stateMachine)
+    {
+        this.stateMachine = stateMachine;
+    }
+    public virtual void Enter() { }
+    public virtual void Update() { }
+    public virtual void Exit() { }
+    protected void ChangeState(State newState)
+    {
+        stateMachine.ChangeState(newState);
+    }
+    public void ChangeState(State nextState, float time)
+    {
+        stateMachine.ChangeState(nextState, time);
+    }
+}
