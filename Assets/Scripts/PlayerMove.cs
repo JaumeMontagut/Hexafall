751757bb835bf5d49ceaa255f444d1f099252d29
JaumeMontagut <jaume.montagut.i.guix@gmail.com>
2021-01-27 22:13:19 +0100

    adjusting before build

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -138,7 +138,7 @@ public class PlayerMove : MonoBehaviour
         //check if its path and if it's not, active the player falling.
         if (!nextPlatform.isPath)
         {
-            Managers.Audio.PlayAudio("Death");
+            //Managers.Audio.PlayAudio("Death");
             playerVars.ActivateFalling();
             nextPlatform.PlayAnimation();
             timeJustFall = Time.time + 0.50F;
